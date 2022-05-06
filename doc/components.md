073e80366eb16ad33dea7d8e4400957f65c00c8c
Candle <candle@candle.me.uk>
2022-05-06 11:28:57 +0100

    some form of events

--- doc/components.md
+++ doc/components.md
@@ -23,8 +23,15 @@ https://reactjs.org/docs/thinking-in-react.html
 # Step 3: Identify the complete minimal set of UI state
 "lots of thinking, not much typing"
 
+1. json/toml text
+2. json/toml parse option
+3. data set
+4. mode
+
 # Step 4: Identify where the state should live
 "lots of thinking, not much typing"
+
+all state in `Content`
 
 # Step 5: Add inverse data flow
 "lots of thinking, not much typing"
--- src/index.js
+++ src/index.js
@@ -32,13 +32,62 @@ class Links extends React.Component {
 }
 
 class Content extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: "{}",
+			text_format: "json",
+			data_set: null,
+			mode: null
+		};
+		this.state_tmp = {
+			text: "{}",
+			text_format: "json",
+			data_set: null,
+			mode: null
+		};
+	}
+
+	update_text(text) {
+		console.log("text update triggered", this, text);
+		this.state_tmp.text = text.target.value;
+	}
+
+	update_text_format(text_format) {
+		console.log("text format update triggered", this, text_format);
+		this.state_tmp.text_format = text_format.target.value;
+	}
+
+	update_data_set(data_set) {
+		console.log("data set update triggered", this, data_set);
+		this.state_tmp.data_set = data_set.target.value;
+	}
+
+	update_mode(mode) {
+		console.log("mode update triggered", this, mode);
+		this.state_tmp.mode = mode.target.value;
+	}
+
+	trigger_render() {
+		console.log("state update triggered", this);
+		this.setState(this.state_tmp);
+	}
+
 	render() {
 		return (
 			<tr>
 				<td>
 					<table><tbody>
 						<tr>
-							<td><Inputs /></td>
+							<td>
+								<Inputs
+									update_text={this.update_text.bind(this)}
+									update_text_format={this.update_text_format.bind(this)}
+									update_data_set={this.update_data_set.bind(this)}
+									update_mode={this.update_mode.bind(this)}
+									trigger_render={this.trigger_render.bind(this)}
+								/>
+							</td>
 							<td><Outputs /></td>
 						</tr>
 					</tbody></table>
@@ -52,19 +101,19 @@ class Inputs extends React.Component {
 	render() {
 		return (
 			<table><tbody>
-				<tr><td><DataSetSelection /></td></tr>
+				<tr><td><DataSetSelection update_data_set={this.props.update_data_set} /></td></tr>
 				<tr>
 					<td>
 						<table><tbody>
 							<tr>
-								<td><InputType /></td>
-								<td><RenderMode /></td>
-								<td><TryItButton /></td>
+								<td><InputType update_text_format={this.props.update_text_format} /></td>
+								<td><RenderMode update_mode={this.props.update_mode} /></td>
+								<td><TryItButton trigger_render={this.props.trigger_render} /></td>
 							</tr>
 						</tbody></table>
 					</td>
 				</tr>
-				<tr><td><InputText /></td></tr>
+				<tr><td><InputText update_text={this.props.update_text} initial_value={"{}"} /></td></tr>
 			</tbody></table>
 		);
 	}
@@ -73,7 +122,7 @@ class Inputs extends React.Component {
 class DataSetSelection extends React.Component {
 	render() {
 		return (
-			<select>
+			<select onChange={this.props.update_data_set} >
 				<option value="dsp" >DSP</option>
 				<option value="vt" >VT</option>
 			</select>
@@ -84,9 +133,9 @@ class DataSetSelection extends React.Component {
 class InputType extends React.Component {
 	render() {
 		return (
-			<select>
+			<select onChange={this.props.update_text_format}>
 				<option value="json" >JSON</option>
-				<option value="toml" >TOML</option>
+				<option disabled={true} value="toml" >TOML</option>
 			</select>
 		);
 	}
@@ -95,7 +144,7 @@ class InputType extends React.Component {
 class RenderMode extends React.Component {
 	render() {
 		return (
-			<select>
+			<select onChange={this.props.update_mode}>
 				<option value="factory-rate" >Factory Rate</option>
 				<option value="manual-rate" >Manual Rate</option>
 			</select>
@@ -106,7 +155,7 @@ class RenderMode extends React.Component {
 class TryItButton extends React.Component {
 	render() {
 		return (
-			<input type="button" value="try-it" />
+			<input type="button" value="try-it" onClick={this.props.trigger_render} />
 		);
 	}
 }
@@ -114,7 +163,7 @@ class TryItButton extends React.Component {
 class InputText extends React.Component {
 	render() {
 		return (
-			<textarea cols="40"></textarea>
+			<textarea cols="40" onChange={this.props.update_text}>{this.props.initial_value}</textarea>
 		);
 	}
 }
